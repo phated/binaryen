3d688e715af244c27632f57d6f52f0e65fdcf156
Alon Zakai <azakai@google.com>
2020-10-08 13:27:32 -0700

    fix

--- src/tools/fuzzing.h
+++ src/tools/fuzzing.h
@@ -468,6 +468,21 @@ private:
   }
 
   void finalizeMemory() {
+    for (auto& segment : wasm.memory.segments) {
+      // If the offset is a global that was imported (which is ok) but no
+      // longer is (not ok) we need to change that.
+      if (auto* offset = segment.offset->dynCast<GlobalGet>()) {
+        if (!wasm.getGlobal(offset->name)->imported()) {
+          // TODO: the segments must not overlap...
+          segment.offset = builder.makeConst(Literal::makeFromInt32(0, Type::i32));
+        }
+      }
+      Address maxOffset = segment.data.size();
+      if (auto* offset = segment.offset->dynCast<Const>()) {
+        maxOffset = maxOffset + offset->value.getInteger();
+      }
+      wasm.memory.initial = std::max(wasm.memory.initial, Address((maxOffset + Memory::kPageSize - 1) / Memory::kPageSize));
+    }
     wasm.memory.initial = std::max(wasm.memory.initial, USABLE_MEMORY);
     if (wasm.memory.max < wasm.memory.initial) {
       wasm.memory.max = wasm.memory.initial;
